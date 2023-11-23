1106144ed4b0491f4ce52605112f6a374bc8acc2
Josephdada01 <ridwandada500@gmail.com>
2023-11-23 07:22:04 +0100

    currently working on the files

--- beautifulbetty_code.c
+++ beautifulbetty_code.c
@@ -20,7 +20,7 @@ int add_numbers(int a, int b)
  *
  * Return: Always 0
  */
-int main(void)
+/*int main(void)
 {
 	int num1 = 10;
 	int num2 = 20;
@@ -30,4 +30,4 @@ int main(void)
 	printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
 	return (0);
-}
+}*/
